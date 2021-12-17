7f60a6a557a75eb02745146ecd47f65ea6bb1091
Laramie Downs <laramiedowns1994@gmail.com>
2021-12-17 13:04:46 -0600

    added automated test. 1 working 1 not

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,21 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('Draw button displays choices', async()=>{
+    await driver.sleep(5000)
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(5000)
+    const choices = await driver.findElement(By.id('choose-header'))
+    const displayed = await choices.isDisplayed()
+    expect(displayed).toBe(true)
+    
+})
+
+test('Add to Duo button adds bot to team', async()=>{
+    await driver.findElement(By.xpath('//div[@class = "bot-card outline"][1]/div[@class = "bot-card outline"]/button[@type = "button"][. = "Add to Duo"]')).click()
+    await driver.sleep(5000)
+    const player = await driver.findElement(By.id('your-duo-header'))
+    const displayed = await player.isDisplayed()
+    expect(displayed).toBe(true)
+})
