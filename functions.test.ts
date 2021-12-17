ba009db7a08998c5a5c7e45143d24b579ac1c00a
Laramie Downs <laramiedowns1994@gmail.com>
2021-12-17 12:02:35 -0600

    unit tests implemented

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,11 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('Array length check', ()=>{
+        expect(shuffleArray([1,2,3,4,5])).toHaveLength(5)
+    }),
+    test('Array contents shuffled', ()=>{
+        expect(shuffleArray([1,2,3,4,5])).not.toBe([1,2,3,4,5])
+    })
+    
 })
